a14511f9daab6d1a578dd9c7c4530ee1ce4c4e93
sitri12 <nite2quake@yahoo.co.uk>
2017-09-10 09:59:03 +0100

    Add files via upload

--- alias.bat
+++ alias.bat
@@ -13,6 +13,16 @@
 
 
 
+set file=alias.config
+if "%1"=="-f" (
+  set file=%2
+  shift
+  shift
+)
+
+if "%1"=="-h" goto usage
+
+
 if "%1"=="-a" goto _add
 if "%1"=="-i" goto _interactive
 if "%1"=="-l" goto _list
@@ -26,7 +36,7 @@ goto _config
   echo Alias		: Command
   echo.
 
-  for /f "tokens=1,2 delims=#" %%a in (alias.config) do (
+  for /f "tokens=1,2 delims=#" %%a in (%file%) do (
     echo %%a		: %%b
 
 
@@ -46,8 +56,9 @@ goto end
   echo or variables in the form of $1 ... $9 for %%1 ... %%9 or $n for all arguments
   echo.
   set /p cmd=Command: 
+  set /p desc=description
 
-  echo %alias%#%cmd%# >> alias.config
+  echo %alias%#%cmd%#%desc% >> %file%
 
   goto end
 
@@ -66,15 +77,52 @@ echo args = %args%
 
 :_write
 
-  echo %2#%3#%args% >> alias.config
+  echo %2#%3#%args% >> %file%
   goto end
 
 :_config
 
-  for /f "tokens=1,2delims=#" %%a in (alias.config) do (
+  for /f "tokens=1,2delims=#" %%a in (%file%) do (
     doskey %%a=%%b $*
 )
+goto end
+:usage
 
+  if "%~2"=="" echo. & call :_GenHlp & goto end
+
+  for /f "tokens=1-3 delims=#" %%a in (%file% ^| findstr "%2") do (
+
+      echo.
+      echo %%a		: %%b
+      echo.
+    if "%%c"=="" goto end
+      echo %%c
+    goto end
+)
+:_GenHlp
+
+
+echo.
+echo Quick and dirty ALIAS command written in Batch for windows
+echo.
+echo USAGE:
+echo.
+echo ALIAS [-A ^<alias^> ^<command^>][-I][-L][-F ^<file^>][-h [^<alias^>]]
+echo.
+echo ALIAS	: Processes the alias.config file and sets up aliases
+echo.
+echo -A	: Create a simple alias with a simple command (eg. ALIAS -A clear cls)
+echo.
+echo -I	: Interactive mode for creating more complex aliases
+echo.
+echo -L	: Lists all aliases and commands in ALIAS.CONFIG file
+echo.
+echo -F	: For using a file other than the default (alias.config)
+echo.
+echo -H	: Displays this page or the description of the named alias
+echo.
+echo NOTE: if invoking -F ^<file^> with other options it must be done first
+)
 :end
 
 exit /b
